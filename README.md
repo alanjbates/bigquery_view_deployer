591cf27c82efa2c6a7fb243f0c206b544df81fdc
alanjbates <38515640+alanjbates@users.noreply.github.com>
2019-07-04 09:12:47 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# bigquery_view_deployer
+# BigQuery View Deployer
+
+This will deploy any .sql in the repo to google cloud platform BigQuery using source control and continuous deployment.
+
+Cloud Build Trigger
+Source code is here: 
+Cloud Build trigger is here: 
+When a commit is merged to branch, cloud build creates a python docker image and runs a .py script which creates/replaces the sql views in the repository.
+master = gcp-project-id-dev
+qa = gcp-project-id-qa
+prod = gcp-project-id-prod
+
+bq_view_deployer.py
+This script loops through each sql file in the repo
+It replaces dynamic variables in the .sql file text:
+_RUN_PROJECT_ID with the current project id eg: gcp-project-id-prod
+_DEPLOYMENT_DTTM is replaced with now() eg: 2019-07-01 21:25:56.516117
+It gets the BigQuery dataset and view name from the .sql filename
+for example take filename:  'weather_data.my_weather_aggregate.sql' 
+dataset_name is the string before the first '.' eg: 'weather_data'
+view_name is the middle string eg: 'my_weather_aggregate'
+For each .sql in the repo, it will create or replace the view automatically.
---
+++ bq_view_deployer.py
@@ -0,0 +1,63 @@
+# Alan Bates BigQuery View Deployer with source control and continuous deployment
+
+import subprocess
+import datetime
+import glob
+import sys
+import os
+
+#install package on ephemeral deployment machine
+subprocess.call([sys.executable, "-m", "pip", "install", "google-cloud-bigquery"])
+from google.cloud import bigquery
+
+#initialize BQ client
+client = bigquery.Client()
+
+#Set run project variable (This is set in your .yaml file)
+run_project_id = os.environ['PROJECT']
+print('Project: ' + run_project_id)
+
+#set deployment dttm variable
+deployment_dttm = datetime.datetime.now()
+
+#Get a list of all files in the working directory and subfolders
+files = glob.glob('*/*',recursive=True)
+
+#Loop through each file in the list
+for sql_file in files:
+    #Only Examine .sql files
+    if sql_file.endswith('.sql'):
+        #get dataset name and view name from filename
+        dataset_name = sql_file.split('.')[0]
+        #if sql file is in a subfolder, drop the foldername
+        if '/' in dataset_name:
+            dataset_name = dataset_name.split('/')[0]
+        #take the viewname from the filename
+        view_name = sql_file.split('.')[1]
+        print('Dataset: ' + dataset_name)
+        print('View: ' + view_name)
+
+        #get sql string
+        sql_string = open(sql_file, 'r').read()
+
+        #update sql string with _RUN_PROJECT_ID
+        sql_string = sql_string.replace('_RUN_PROJECT_ID', run_project_id)
+        #update sql string with _DEPLOYMENT_DTTM
+        sql_string = sql_string.replace('_DEPLOYMENT_DTTM', str(deployment_dttm))
+
+        #set BQ client params
+        source_dataset_id = ''
+        source_table_id = ''
+        shared_dataset_ref = client.dataset(dataset_name)
+        view_ref = shared_dataset_ref.table(view_name)
+        view = bigquery.Table(view_ref)
+        sql_template = (sql_string)
+        view.view_query = sql_template.format(run_project_id, source_dataset_id, source_table_id)
+
+        #Create/Replace View
+        try:
+            view = client.update_table(view, ["view_query"])
+            print('REPLACED VIEW: ' + dataset_name + '.' + view_name)
+        except:
+            view = client.create_table(view, ["view_query"])
+            print('CREATED VIEW: ' + dataset_name + '.' + view_name)
